e429fef4a9a42841d5a1ee4451f70ea61c60c85b
Chris Cheng <yifan.cheng.1998@gmail.com>
2024-03-10 13:17:44 +1100

    query static customers

---
+++ graphql/schema.ts
@@ -0,0 +1,27 @@
+// import { builder } from "./builder";
+
+// export const schema = builder.toSchema()
+
+export const typeDefs = `
+  type Link {
+    id: ID
+    title: String
+    description: String
+    url: String
+    category: String
+    imageUrl: String
+    users: [String]
+  }
+
+  type Customer {
+    id:        ID    
+    name:      String     
+    email:     String     
+    image_url: String     
+  }
+
+  type Query {
+    links: [Link]!,
+    customers: [Customer]!,
+  }
+`
